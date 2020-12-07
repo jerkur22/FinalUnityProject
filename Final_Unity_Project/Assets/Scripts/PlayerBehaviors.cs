a947fb2866dff9e117fcf47393ea18852246cf17
ChrisK197 <chrkal22@bergen.org>
2020-12-06 20:50:59 -0500

    Added spongebob spin function with arrow keys

---
+++ Final_Unity_Project/Assets/Scripts/PlayerBehaviors.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviors : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    private int degree;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        degree = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnEnable()
+    {
+        StartCoroutine(RotateSpongebob());
+    }
+
+    public void OnDisable()
+    {
+        StopCoroutine(RotateSpongebob());
+    }
+
+    public IEnumerator RotateSpongebob()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds((float)0.1);
+
+            if (Input.GetKey("left"))
+            {
+                degree += 5;
+                transform.eulerAngles = Vector3.forward * degree;
+            }
+            if (Input.GetKey("right"))
+            {
+                degree -= 5;
+                transform.eulerAngles = Vector3.forward * degree;
+            }
+        }
+
+    }
+}
